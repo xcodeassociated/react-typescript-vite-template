f324a31ef1169667b9f8eaaad3c2c38996b3ec74
xcodeassociated <janusz.majchrzak@protonmail.com>
2024-07-12 17:30:53 +0200

    add theme-provider and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import '@/App.css'
-import {Button} from "@/components/ui/button.tsx";
+import { Button } from '@/components/ui/button.tsx'
+import { ModeToggle } from '@/components/custom/mode-toggle.tsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,16 +20,13 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
+        <Button onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <ModeToggle />
     </>
   )
 }
