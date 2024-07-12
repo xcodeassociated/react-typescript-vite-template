d597bff9034a907530d75566003f2c015450ce3a
xcodeassociated <janusz.majchrzak@protonmail.com>
2024-07-12 17:52:46 +0200

    add missing eslint prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,10 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
+    // This disables the formatting rules in ESLint that Prettier is going to be responsible for handling.
+    // Make sure it's always the last config, so it gets the chance to override other configs.
+    'eslint-config-prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
