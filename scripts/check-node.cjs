5f72f27d7c72a020bf174e563709e8a19711ca88
xcodeassociated <janusz.majchrzak@protonmail.com>
2026-02-04 16:22:32 +0100

    Remove node version check script and restore original scripts

--- scripts/check-node.cjs
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-const semver = require('semver')
-const version = process.version
-if (!semver.satisfies(version, '>=22.12.0')) {
-  console.error(`Node version ${version} is not supported. Please use Node >=22.12.0`)
-  process.exit(1)
-}
