2b9a4c638d3d2e14a6539cd434f4b82e8c2fbfe0
xcodeassociated <janusz.majchrzak@protonmail.com>
2026-02-04 15:34:41 +0100

    Upgrade dependencies: Radix UI, testing libs, GraphQL, utilities, TypeScript, Vite, Tailwind v4

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwind(), tsconfigPaths()],
+  // @ts-ignore
   test: {
     globals: true,
     environment: 'jsdom',
